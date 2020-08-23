787b86e42e1e4505716c341bdfdfe5911813cb0d
luisop <luisfop77@gmail.com>
2020-08-23 19:46:58 -0300

    added code without refatctoring

--- src/HOC/WithCounter.js
+++ src/HOC/WithCounter.js
@@ -22,7 +22,7 @@ export default function withCounter (WrappedComponent) {
         //     return { counter: prevState.counter + 1 }
         // })  
 
-        this.setState({counter: counter + 1})
+        this.setState({counter: this.counter + 1})
     };
 
     render() { 
@@ -30,3 +30,54 @@ export default function withCounter (WrappedComponent) {
     }
   };
 } 
+
+
+
+// WITHOUT REFACTORING
+
+// import React, { Component } from 'react';
+
+// export default function withCounter (WrappedComponent) {
+//   return class extends Component {
+    
+//     constructor(props){
+//         super(props)
+
+//         this.state = {
+//             counter: 0,
+//             name: 'Luis Felipe',
+//             arrayOfThing: []
+//         }
+//     }
+
+//     handleName = () => {
+//         this.setState({name: 'Raquel'})
+//     }
+
+
+//     incrementCounter = () => {
+
+//         console.log(this.state.counter)
+
+//         if (this.state.counter >= 0 && this.state.counter <= 10) {
+
+//           this.setState({ 
+//             counter: this.state.counter + 1
+//           })
+
+//           this.setState({arrayOfThing: [...this.state.arrayOfThing, 44]})
+
+//         } else {
+//           this.setState({
+//             counter: 0
+//           })
+//         }
+
+//         console.log(this.state.arrayOfThing)
+//     };
+
+//     render() { 
+//       return <WrappedComponent counter={this.state.counter} incrementCounter={this.incrementCounter} name={this.state.name} nameHandler={this.handleName} />
+//     }
+//   };
+// } 
